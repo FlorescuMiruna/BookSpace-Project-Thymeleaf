af8a36b6d9038686616b9e31f4b916e1a6766301
FlorescuMiruna <mirunastefaniaflorescu@gmail.com>
2024-07-07 20:00:54 +0300

    code changes

--- BookSpace/src/main/java/com/example/bookspace/controller/AuthorController.java
+++ BookSpace/src/main/java/com/example/bookspace/controller/AuthorController.java
@@ -6,6 +6,7 @@ import com.example.bookspace.model.AuthorDetails;
 import com.example.bookspace.service.AuthorService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +14,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 @Controller
@@ -97,4 +99,5 @@ public class AuthorController {
         modelAndView.setViewName("notFoundException");
         return modelAndView;
     }
+
 }
--- BookSpace/src/main/java/com/example/bookspace/service/AuthorService.java
+++ BookSpace/src/main/java/com/example/bookspace/service/AuthorService.java
@@ -3,6 +3,9 @@ package com.example.bookspace.service;
 import com.example.bookspace.model.Author;
 import com.example.bookspace.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
